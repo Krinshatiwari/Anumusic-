86235fc9b33b89677fc9ebf9162dd76158a54f25
Krishna-The-Fucker <pk4108644@gmail.com>
2026-07-27 18:30:40 +0530

    Update UPSTREAM_REPO URL in config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tiwariji890/Anumusic-",
+    "https://github.com/Krishna-The-Fucker/Anumusic-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Anu")
 GIT_TOKEN = getenv(
