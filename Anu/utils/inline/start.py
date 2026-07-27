913fb325fe43704beb9906383c118aabb4b017f2
Krishna-The-Fucker <pk4108644@gmail.com>
2026-07-27 10:52:14 +0530

    Update start.py

--- Anu/utils/inline/start.py
+++ Anu/utils/inline/start.py
@@ -14,7 +14,7 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url="https://github.com/tiwariji890/Anumusic-"
+                url="https://files.catbox.moe/c30eoi.mp4"
             )
         ]
     ]
@@ -41,7 +41,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
-            InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/tiwariji890/Anumusic-")
+            InlineKeyboardButton(text=_["S_B_5"], url="https://files.catbox.moe/c30eoi.mp4")
         ]
     ]
     return buttons
